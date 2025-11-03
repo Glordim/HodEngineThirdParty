58065d127c31dca8b6ce0d86f7a07a716b54b4c3
Glordim <anthony.tuffraud@gmail.com>
2025-11-03 09:57:42 +0100

    Oops fix Release config

--- build_macos_arm64.sh
+++ build_macos_arm64.sh
@@ -1,3 +1,3 @@
-cmake -G Ninja -S . -B Build/MacOs_arm64 -DCMAKE_INSTALL_PREFIX=Install/MacOs_arm64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake --build Build/MacOs_arm64 --config Release --parallel
-cmake --install Build/MacOs_arm64 --config Release
+cmake -G Ninja -S . -B Build/MacOs_arm64 -DCMAKE_INSTALL_PREFIX=Install/MacOs_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake --build Build/MacOs_arm64 --parallel
+cmake --install Build/MacOs_arm64
--- build_win_x64.bat
+++ build_win_x64.bat
@@ -1,3 +1,3 @@
-cmake -G Ninja -S . -B Build/Win_x64 -DCMAKE_INSTALL_PREFIX=Install/Win_x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake --build Build/Win_x64 --config Release --parallel
-cmake --install Build/Win_x64 --config Release
+cmake -G Ninja -S . -B Build/Win_x64 -DCMAKE_INSTALL_PREFIX=Install/Win_x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake --build Build/Win_x64 --parallel
+cmake --install Build/Win_x64
