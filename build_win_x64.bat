0180a9ee99fe1b5634a615f07ab16b180ba3f188
Glordim <anthony.tuffraud@gmail.com>
2026-03-08 11:33:45 +0100

    Windows: use command line build tools instead of visual studio

--- build_win_x64.bat
+++ build_win_x64.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 cmake -G Ninja -S . -B Build/Win_x64 -DCMAKE_INSTALL_PREFIX=Install/Win_x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe
 cmake --build Build/Win_x64 --parallel
