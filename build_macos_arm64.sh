baaf723416eee99f4ad38a66a5823c6b1b7ad3ca
Glordim <anthony.tuffraud@gmail.com>
2026-03-08 11:36:03 +0100

    Upgrade Freetype

--- build_macos_arm64.sh
+++ build_macos_arm64.sh
@@ -1,3 +1,3 @@
-cmake -G Ninja -S . -B Build/MacOs_arm64 -DCMAKE_INSTALL_PREFIX=Install/MacOs_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake -G Ninja -S . -B Build/MacOs_arm64 -DCMAKE_INSTALL_PREFIX=Install/MacOs_arm64 -DCMAKE_BUILD_TYPE=Release
 cmake --build Build/MacOs_arm64 --parallel
 cmake --install Build/MacOs_arm64
--- build_win_x64.bat
+++ build_win_x64.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-cmake -G Ninja -S . -B Build/Win_x64 -DCMAKE_INSTALL_PREFIX=Install/Win_x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe
+cmake -G Ninja -S . -B Build/Win_x64 -DCMAKE_INSTALL_PREFIX=Install/Win_x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe
 cmake --build Build/Win_x64 --parallel
 cmake --install Build/Win_x64
